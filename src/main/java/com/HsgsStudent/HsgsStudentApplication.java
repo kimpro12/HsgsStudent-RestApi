5d96e4d92653a210d75044e253bdd29e0b9d7a12
kimbro <95573884+kimpro12@users.noreply.github.com>
2023-10-20 21:54:02 +0700

    Add files via upload

---
+++ src/main/java/com/HsgsStudent/HsgsStudentApplication.java
@@ -0,0 +1,13 @@
+package com.HsgsStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsgsStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsgsStudentApplication.class, args);
+	}
+
+}
